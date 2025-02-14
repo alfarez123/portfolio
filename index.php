737956f1dee9337d27f233be54bbe02b3795fb86
alfarez123 <antonioalfarez88@gmail.com>
2025-02-14 13:11:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>portfolio</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+   <!-- container -->
+   <div class="container-fluid">
+    <div class="navbar">
+      <div class="row">
+        <div class="col-sm-6">
+          <h1>My Portfolio</h1>
+        </div>
+        <div class="col-sm-6">
+          <ul class="text-right">
+            <li><a href="#profil">Profile</a></li>
+            <li><a href="#pengalaman">Work Experience</a></li>
+            <li><a href="#kontak">Contact</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- jumbotron -->
+      <div class="jumbotron">
+        <div class="row">
+          <div class="col-sm-6 text-center">
+            <h1>Al Farez Hidayat</h1>
+            <h2>S1-Information Technology</h2>
+            <hr>
+            <p>Web Developer | System Analyst | Algorithm | Graphic Design </p>
+          </div>
+          <div class="col-sm-6 text-center">
+            <img src="img/poto.jpg" class="img-circle">
+          </div>
+        </div>
+      </div>
+      <!-- akhir jumbotron -->
+      
+      <!-- profile & pendidikan-->
+      <div class="profile" id="profile">
+        <div class="row">
+          <div class="col-sm-4 col-sm-offset-2 text-justify">
+            <h2 class="text-center">Profile</h2>
+            <hr>
+            <p>Have educational provisions throughout
+                four years to graduate as a bachelor
+                computers in 2023 with
+                GPA score of 3.68 at Mitra University.</p>
+          </div>
+          <div class="col-sm-4">
+            <h2 class="text-center">Education</h2>
+            <hr>
+            <ul>
+              <li>S1 Universitas Mitra | 2019-2023</li>
+              <li>SMK Yadika | 2015-2018</li>
+              <li>MTS Guppi | 2012-2015</li>
+              <li>SDN6 Merak Batin Natar | 2006-2012</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- akhir profile & pendidikan -->
+
+      <!--  pengalaman organisasi dan kerja -->
+      <div class="dua text-center text-justify">
+        <div class="row">
+          <div class="col-sm-4 col-sm-offset-2">
+            <h2 class="text-center">Ability</h2>
+            <hr>
+            <h4>Microsoft Word</h4>
+            <div class="row">
+              <div class="col-sm-12">
+                <ul>
+                    <li>Word</li>
+                    <li>Power Point</li>
+                    <li>Excel (vlookup, hlookup, mid, right, left, match, max, win, sum, average, countif, if, pivot)</li>
+                  </ul>
+              </div>
+            </div>
+
+            <h4>Desain Grafis</h4>
+            <div class="row">
+                <div class="col-sm-6">
+                  <ul>
+                    <li>Photoshop</li>
+                    <li>Corel draw</li>
+                    <li>Canva</li>
+                    <li>Filmora</li>
+                  </ul>
+                </div>
+                <div class="col-sm-6">
+                  <ul>
+                    <li>Adobe Ilustrator</li>
+                    <li>Notepad</li>
+                    <li>Sublime Text</li>
+                    <li>Visual Code</li>
+                  </ul>
+                </div>
+              </div>
+              <h4>Pemrograman</h4>
+              <div class="row">
+                <div class="col-sm-6">
+                  <ul>
+                    <li>Html</li>
+                    <li>Css</li>
+                    <li>Javascript</li>
+                  </ul>
+                </div>
+                <div class="col-sm-6">
+                  <ul>
+                    <li>Php</li>
+                    <li>Mysql</li>
+                    <li>Bootstrap</li>
+                  </ul>
+                </div>
+              </div>
+          </div>
+          <div class="col-sm-4" id="pengalaman">
+            <h2 class="text-center">Work Experience</h2>
+            <hr>
+            <h4 class="text-left">Sales Kordinator PT.Tunas Honda | 2023</h4>
+            <ul>
+              <li>Melakukan pengawasan pada penjualan</li>
+              <li>Mengkordinir sesuai area</li>
+              <li>Melakukan survey lapangan</li>
+            </ul>
+            <h4 class="text-left">IT Support PT PLN | 2022 (apprenticeship)</h4>
+            <ul>
+              <li>Network system development</li>
+              <li>Software and hardware installation</li>
+              <li>Trouble shooting</li>
+              <li>Website Design</li>
+            </ul>
+            <h4 class="text-left">Mekanik Mobil | 2019</h4>
+            <ul>
+              <li>Mendiagnosis Masalah</li>
+              <li>Perbaikan dan Pemeliharaan</li>
+              <li>Penggantian dan Perbaikan Komponen</li>
+              <li>Pengaturan dan Tune-Up</li>
+            </ul>
+            <h4 class="text-left">Waiters | 2018</h4>
+            <ul>
+              <li>Menyambut dan Melayani Pelanggan</li>
+              <li>Mengambil Pesanan</li>
+              <li>Menyajikan Makanan dan Minuman</li>
+              <li>Menjaga Kebersihan dan Penataan Meja</li>
+              <li>Menanggapi Permintaan Pelanggan</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- akhir pengalaman organisasi dan kerja -->
+
+      <!-- kemampuan dan kontak -->
+      <div class="tiga">
+        <div class="row">
+          <div class="col-sm-4 col-sm-offset-2 text-justify">
+            <h2 class="text-center">Organizational Experience</h2>
+            <hr>
+              <ul>
+                <li>Himpunan Mahasiswa</li>
+                <li>Osis</li>
+                <li>Risma</li>
+            </ul>
+            </div>
+
+          <!-- kontak -->
+          <div class="col-sm-4">
+            <h2 class="text-center" id="kontak">Contact</h2>
+            <hr>
+            <ul>
+              <li class="glyphicon glyphicon-envelope"> antonioalfarez88@gmail.com</li>
+              <li class="glyphicon glyphicon-earphone"> 089652469013 | 085269066347</li>
+              <li class="glyphicon glyphicon-map-marker"> Natar Lampung Selatan 35362</li><br>
+              <li class="glyphicon glyphicon-globe"> alfarez_hidayatt</li>
+            </ul>
+          </div>
+        </div>
+        <!-- kemampuan dan kontak -->
+
+        <div class="image">
+          <div class="row">
+            <div class="col-sm-12 text-center" id="portfolio">
+              <h2 style="color: white">Portfolio</h2>
+              <hr>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-4 text-center">
+              <img src="img/1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300px">            </div>
+            <div class="col-sm-4 text-center">
+              <img src="img/2.jpg" class="img-thumbnail" alt="Cinque Terre" width="300px"> 
+            </div>
+            <div class="col-sm-4 text-center">
+              <img src="img/3.jpg" class="img-thumbnail" alt="Cinque Terre" width="300px"> 
+            </div>
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-sm-4 text-center">
+              <img src="img/4.jpg" class="img-thumbnail" alt="Cinque Terre" width="300px">            </div>
+            <div class="col-sm-4 text-center">
+              <img src="img/5.jpg" class="img-thumbnail" alt="Cinque Terre" width="300px"> 
+            </div>
+            <div class="col-sm-4 text-center">
+              <img src="img/6.jpg" class="img-thumbnail" alt="Cinque Terre" width="300px"> 
+            </div>
+            <br><br>
+          </div>
+        </div>
+
+        <div class="footer">
+          <h4>&copy;Copyright2024alfarezhidayat</h4>
+        </div>
+   </div>
+    <!-- akhir container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  </div>
+  </body>
+</html>
